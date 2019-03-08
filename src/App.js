8bee6b5f7416b4db1157d83478588426b08ae6c1
dearmcw <dearmcw0070@gmail.com>
2019-03-08 16:36:18 +0900

    초가 오를때마다 페이지네이션도 연동되는 기본세팅

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = {
+    currentPage: 1,
+    currentNearPages: [1,2,3,4,5,6,7,8,9,10],
+  }
+
+  componentDidMount() {
+    setInterval(()=> {
+      this.setState({
+        currentPage: this.state.currentPage + 1,
+      });
+    }, 1000)
+  } 
+  
   render() {
     return (
-      <div className="App">
-        하하하
+      <div style={{textAlign:'center'}}>
+        {this.state.currentPage}
+        <hr/>
+        {this.state.currentNearPages.map((item, i) => {
+        return <div key={i} style={{display:'inline-block', marginRight:'5px'}}>
+          {item === this.state.currentPage
+            ? <strong>{item}</strong>
+            : <span>{item}</span>
+          }
+        </div>
+        })}
       </div>
     );
   }
